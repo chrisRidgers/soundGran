696d4fe40db3b9667c83652d8d3f52d8a770beb5
Chris Ridgers <chrisridgers@digiridge.co.uk>
2014-03-07 12:14:02 +0000

    wav2aiff implemented

--- Makefile
+++ Makefile
@@ -1,3 +1,9 @@
+INCLUDE= -I include/
+LIB = -L lib/
+
+
 default:
 	make -C portsf install
 	make -C portsf/ veryclean
+	gcc -c -o v6c13.o $(INCLUDE) v6c13.c
+	gcc $(LIB) -o v6c13.out v6c13.o  lib/libportsf.a
---
+++ v6c13.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <portsf.h>
+
+enum{ARG_NAME, ARG_INPUT, ARG_OUTPUT, ARG_DUR, ARG_GRAINDUR, ARGC};
+
+int main(int argc, char *argv[])
+{
+	PSF_PROPS props;
+	int infile;
+	int outfile;
+	float *buffer;
+	long num_frames;
+
+	float duration;
+	float grain_duration;
+
+	if(argc!=ARGC)
+	{
+		printf("Please use wav2aif as: wav2aif INPUT_WAV OUTPUT_AIF\n");
+		return 1;
+	}
+
+	duration = atof(argv[ARG_DUR]);
+	grain_duration = atof(argv[ARG_GRAINDUR]);
+
+	if(psf_init())
+	{
+		printf("Error: unable to open portsf\n");
+		return 1;
+	}
+
+	infile = psf_sndOpen(argv[ARG_INPUT], &props, 0);
+	if(infile<0)
+	{
+		printf("Error, unable to read %s\n", argv[ARG_INPUT]);
+		return 1;
+	}
+
+	outfile = psf_sndCreate(argv[ARG_OUTPUT], &props, 0, 0, PSF_CREATE_RDWR);
+	if(outfile<0)
+	{
+		printf("Unable to create %s\n", argv[ARG_OUTPUT]);
+		return 1;
+	}
+
+	float totalDur;
+
+	for(totalDur = 0.0f; totalDur < duration; totalDur += grain_duration)
+	{
+		num_frames = (long)grain_duration*props.srate;
+		buffer = (float*)malloc(num_frames*props.chans*sizeof(float));
+		if(buffer==NULL)
+		{
+			printf("Error unable to allocate buffer\n");
+			if(psf_sndClose(infile))
+			{
+				printf("Warning: error closing %s\n", argv[ARG_INPUT]);
+			}
+			if(psf_sndClose(outfile))
+			{
+				printf("Warning: error closing %s\n", argv[ARG_OUTPUT]);
+			}
+			return 1;
+		}
+		printf("Reading %s to buffer \n", argv[ARG_INPUT]);
+
+		if(psf_sndReadFloatFrames(infile, buffer, num_frames) !=num_frames)
+		{
+			printf("Warning: error writing %s\n", argv[ARG_OUTPUT]);
+			return 1;
+		}
+
+		//add attack and decay to buffer
+
+		printf("Writing %s ... \n", argv[ARG_OUTPUT]);
+		if(psf_sndWriteFloatFrames(outfile, buffer, num_frames) != num_frames)
+		{
+			printf("Warning: error writing %s\n", argv[ARG_OUTPUT]);
+			return 1;
+		}
+
+	}
+
+	if(infile >= 0)
+	{
+		if(psf_sndClose(infile))
+		{
+			printf("Warning error closing %s\n", argv[ARG_INPUT]);
+		}
+	}
+	if(outfile >= 0)
+	{
+		if(psf_sndClose(outfile))
+		{
+			printf("Warning error closing %s\n", argv[ARG_OUTPUT]);
+		}
+		printf("Closed fine \n");
+	}
+
+	psf_finish();
+	printf("Completed format conversion\n");
+
+	return 0;
+}
+
+
